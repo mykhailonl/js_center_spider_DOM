ec0bc1657801345719a389b52c40b4024101fe4a
Mykhailo Lapchynskyi <mykhailolapchynskyi@mac.local>
2024-09-29 15:40:35 +0200

    make build

---
+++ index.2aa78a8c.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=t.parentElement,c=e.clientWidth,i=e.clientHeight,l=t.offsetWidth,n=t.offsetHeight;t.style.left="".concat((c-l)/2,"px"),t.style.top="".concat((i-n)/2,"px");
+//# sourceMappingURL=index.2aa78a8c.js.map
